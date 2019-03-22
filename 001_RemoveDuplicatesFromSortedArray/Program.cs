2a485e91ee811d8e4f58cc91914eabb8041aa6de
ArtemenkoArt <ArtemenkoArt82@users.noreply.github.com>
2019-03-22 14:05:34 +0200

    Publish the 1st algorithm

--- 001_RemoveDuplicatesFromSortedArray/Program.cs
+++ 001_RemoveDuplicatesFromSortedArray/Program.cs
@@ -8,8 +8,43 @@ namespace _001_RemoveDuplicatesFromSortedArray
 {
     class Program
     {
+        public static int removeDuplicates(ref int[] A)
+        {
+            if (A.Length < 2)
+                return A.Length;
+
+            int j = 0;
+            int i = 1;
+            while (i < A.Length)
+            {
+                if (A[i] != A[j])
+                {
+                    j++;
+                    A[j] = A[i];
+                }
+                i++;
+            }
+            return j + 1;
+        }
+
+        static void PrintArray(int[] A, string message)
+        {
+            Console.WriteLine("");
+            Console.Write("{0}", message);
+            for (int i = 0; i < A.Length; i++)
+            {
+                Console.Write("{0}, ", A[i]);
+            }
+        }
+
         static void Main(string[] args)
         {
+            int[] M = {1,1,1,2,2,3,4,4,6,8,7,7,7};
+            int n = 0;
+            PrintArray(M, "Array before change: ");
+            n = removeDuplicates(ref M);
+            PrintArray(M, "Array after change: ");
+            Console.ReadKey();
         }
     }
 }
